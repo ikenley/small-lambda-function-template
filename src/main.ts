4e29ccd42e1f8c142942c2721261f8f305030d60
ikenley <ikenley6@gmail.com>
2022-09-17 19:31:41 -0400

    init TODO deploy manually

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { S3Event } from "aws-lambda";
+
+export const handler = async (event: S3Event) => {
+  console.log("event", event)
+  
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
